7273104db5112e6870710b8bf9cb44c35235bcca
Jacob Grahn <jacob@grahn.io>
2024-04-28 00:46:39 -0400

    initial client config

---
+++ client/main/Main.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+const Title = preload("res://pages/title/Title.tscn")
+var current_scene: Node2D
+
+
+func _ready():
+	set_scene("Title")
+
+
+func set_scene(scene_name: String):
+	if current_scene:
+		current_scene.queue_free()
+	current_scene = self[scene_name].instantiate()
+	add_child(current_scene)
