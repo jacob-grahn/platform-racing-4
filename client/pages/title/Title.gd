69a4b1d7d7dfa6588fea5a20c431babd4a30242d
Jacob Grahn <jacob@grahn.io>
2024-05-01 15:48:21 -0400

    gwibble is back!

--- client/pages/title/Title.gd
+++ client/pages/title/Title.gd
@@ -1,11 +1,5 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	$Label.rotation_degrees += delta * 50
