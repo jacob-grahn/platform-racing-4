19bc17ff2b32ba79ac2e0421a26f22334106cfa7
Raul <47065615+ctrlraul@users.noreply.github.com>
2024-10-15 12:01:00 -0300

    Fixes warnings for Cursor.gd

--- client/pages/editor/cursors/Cursor.gd
+++ client/pages/editor/cursors/Cursor.gd
@@ -16,11 +16,12 @@ var current_cursor: Node2D
 
 func _ready():
 	var editor_menu = get_node("../EditorMenu")
-	editor_menu.connect("control_event", _on_control_event)
-	control.connect("gui_input", _on_gui_input)
+	editor_menu.control_event.connect(_on_control_event)
+	control.gui_input.connect(_on_gui_input)
 	
 	for child in get_children():
-		child.connect("level_event", _on_subcursor_event)
+		if child.has_signal("level_event"):
+			child.level_event.connect(_on_subcursor_event)
 
 
 func _on_gui_input(event: InputEvent):
@@ -28,8 +29,8 @@ func _on_gui_input(event: InputEvent):
 		using_gui = false
 
 
-func _process(delta):
-	if Input.is_mouse_button_pressed(1) && !using_gui: # Left click
+func _process(_delta):
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) && !using_gui: # Left click
 		if !mouse_down:
 			current_cursor.on_mouse_down()
 			mouse_down = true
@@ -58,5 +59,5 @@ func _on_control_event(event: Dictionary) -> void:
 
 
 func _on_subcursor_event(event: Dictionary) -> void:
-	emit_signal("level_event", event)
+	level_event.emit(event)
 	
