8487bf3e60e0fa5f98aecf24d25d47d2e53155c6
Raul <47065615+ctrlraul@users.noreply.github.com>
2024-10-16 17:28:48 -0300

    Vanish block refactor and fix

--- client/tiles/vanish/Vanish.gd
+++ client/tiles/vanish/Vanish.gd
@@ -10,29 +10,35 @@ func init():
 	any_side.push_back(vanish)
 	is_safe = false
 
-func vanish(player: Node2D, tilemap: TileMap, coords: Vector2i):
+
+func vanish(_player: Node2D, tilemap: TileMap, coords: Vector2i):
+	
 	var atlas_coords = tilemap.get_cell_atlas_coords(0, coords)
+	
 	if atlas_coords == Vector2i(-1, -1):
 		return
-	if try_reinit(coords): # True: spawn new instance, False: animation is playing, attempt to despawn it early
+	
+	if tilemap.get_cell_alternative_tile(0, coords) == 1:
 		return
-
+	
+	if vanish_effects.has(coords):
+		vanish_effects.get(coords).vanish_again()
+		return
+	
 	tilemap.set_cell(0, coords, 0, atlas_coords, 1)
+	
 	var tile_atlas = tilemap.tile_set.get_source(0).texture
 	var vanish_effect = VANISH_EFFECT.instantiate()
-	vanish_effect.position = coords * Settings.tile_size
+	
 	tilemap.add_child(vanish_effect)
+	
 	vanish_effect.init(self, tile_atlas, atlas_coords, tilemap, coords)
+	vanish_effect.position = coords * Settings.tile_size
+	
 	add_to_vanish_dict(coords, vanish_effect)
 
-func try_reinit(coords: Vector2i) -> bool:
-	var vanish_effect = vanish_effects.get(coords)
-	if vanish_effect == null:
-		return false
-	vanish_effect._attempt_despawn_early()
-	return true
 
-func add_to_vanish_dict(coords: Vector2i, vanish_effect):
+func add_to_vanish_dict(coords: Vector2i, vanish_effect: VanishEffect) -> void:
 	vanish_effects[coords] = vanish_effect
 
 func remove_from_vanish_dict(coords: Vector2i) -> void:
