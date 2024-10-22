acc55db2bc074c5509b9845b25ffe030da92552d
Jeremiah <jqrqmiqh@gmail.com>
2024-10-22 19:22:43 -0400

    Add files via upload

--- client/character/Stats.gd
+++ client/character/Stats.gd
@@ -1,17 +1,19 @@
 class_name Stats
 
-var jump: int = 5
-var speed: int = 5
-var skill: int = 5
+var jump: int = 0
+var speed: int = 0
+var accel: int = 0
+var skill: int = 0
 
 
 func get_jump_bonus() -> float:
 	return 1 + (jump / 20.0)
 
-
 func get_speed_bonus() -> float:
 	return 1 + (speed / 20.0)
-
+	
+func get_accel_bonus() -> float:
+	return 1 + (accel / 20.0)
 
 func get_skill_bonus() -> float:
 	return 1 + (skill / 20.0)
@@ -20,15 +22,17 @@ func get_skill_bonus() -> float:
 func inc_all(num: int) -> void:
 	jump += num
 	speed += num
+	accel += num
 	skill += num
 	enforce_limits()
 
 
 func enforce_limits() -> void:
-	jump = clamp(jump, 0, 10)
-	speed = clamp(speed, 0, 10)
-	skill = clamp(skill, 0, 10)
+	jump = clamp(jump, -10, 10)
+	speed = clamp(speed, -10, 10)
+	accel = clamp(accel, -10, 10)
+	skill = clamp(skill, -10, 10)
 
 
 func get_total() -> int:
-	return jump + speed + skill
+	return jump + speed + accel + skill
