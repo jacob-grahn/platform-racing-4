63db0b8804fbb1dcff472a1a32262d5198b72315
Jacob Grahn <jacob@grahn.io>
2024-11-02 00:32:24 -0400

    more work on talking to a socket server

---
+++ client/game_client/GameClient.gd
@@ -0,0 +1,57 @@
+extends Node2D
+
+var websocket_url = "ws://localhost:8080/ws"
+var socket = WebSocketPeer.new()
+
+
+func _ready() -> void:
+	# Initiate connection to the given URL.
+	var err = socket.connect_to_url(websocket_url)
+	if err != OK:
+		print("Unable to connect")
+		set_process(false)
+	else:
+		# Wait for the socket to connect.
+		await get_tree().create_timer(2).timeout
+
+		# Send data.
+		var data = {
+			"id": "1",
+			"ms": 5938,
+			"pos": {
+				"x": 1,
+				"y": 2,
+				"vx": 3,
+				"vy": 4
+			}
+		}
+		
+		socket.send_text(JSON.stringify(data))
+
+
+func _process(delta: float) -> void:
+	# Call this in _process or _physics_process. Data transfer and state updates
+	# will only happen when calling this function.
+	socket.poll()
+
+	# get_ready_state() tells you what state the socket is in.
+	var state = socket.get_ready_state()
+
+	# WebSocketPeer.STATE_OPEN means the socket is connected and ready
+	# to send and receive data.
+	if state == WebSocketPeer.STATE_OPEN:
+		while socket.get_available_packet_count():
+			print("Got data from server: ", socket.get_packet().get_string_from_utf8())
+
+	# WebSocketPeer.STATE_CLOSING means the socket is closing.
+	# It is important to keep polling for a clean close.
+	elif state == WebSocketPeer.STATE_CLOSING:
+		pass
+
+	# WebSocketPeer.STATE_CLOSED means the connection has fully closed.
+	# It is now safe to stop polling.
+	elif state == WebSocketPeer.STATE_CLOSED:
+		# The code will be -1 if the disconnection was not properly notified by the remote peer.
+		var code = socket.get_close_code()
+		print("WebSocket closed with code: %d. Clean: %s" % [code, code != -1])
+		set_process(false) # Stop processing.
--- game-server/main.go
+++ game-server/main.go
@@ -11,9 +11,9 @@ var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 	CheckOrigin: func(r *http.Request) bool {
-		origin := r.Header.Get("Origin")
-		return origin == "http://127.0.0.1:8080" || origin == "https://platformracing.com" || origin == "https://dev.platformracing.com"
-		// todo: do we care about this? should we allow all origins?
+		// origin := r.Header.Get("Origin")
+		// return origin == "http://127.0.0.1:8080" || origin == "https://platformracing.com" || origin == "https://dev.platformracing.com"
+		return true // allow all origins
 	},
 }
 
@@ -21,12 +21,12 @@ var clients = make(map[*websocket.Conn]bool)
 var sendQueue = make(chan Update)
 
 type Update struct {
-	ID        string           `json:"id"`   // user id
-	Time      int              `json:"time"` // client time in milliseconds since level started
-	Position  *PositionUpdate  `json:"position,omitempty"`
-	Property  []PropertyUpdate `json:"property,omitempty"`
-	Block     []BlockUpdate    `json:"block,omitempty"`
-	Character *CharacterUpdate `json:"character,omitempty"`
+	ID   string                 `json:"id"`             // user id
+	MS   int                    `json:"ms"`             // client time in milliseconds since level started
+	Pos  *PositionUpdate        `json:"pos,omitempty"`  // position
+	Val  map[string]interface{} `json:"val,omitempty"`  // property values
+	Tile []TileUpdate           `json:"tile,omitempty"` // tile updates
+	Char *CharacterUpdate       `json:"char,omitempty"` // character values, stats, look, etc
 }
 
 type PositionUpdate struct {
@@ -36,12 +36,7 @@ type PositionUpdate struct {
 	VY int `json:"vy"` // y velocity
 }
 
-type PropertyUpdate struct {
-	K string `json:"k"` // key
-	V string `json:"v"` // value
-}
-
-type BlockUpdate struct {
+type TileUpdate struct {
 	X     int    `json:"x"`     // tile x
 	Y     int    `json:"y"`     // tile y
 	Layer int    `json:"layer"` // layer id
@@ -69,7 +64,7 @@ func main() {
 }
 
 func handleHealth(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "ok")
+	fmt.Println(w, "ok")
 }
 
 func handleWS(w http.ResponseWriter, r *http.Request) {
@@ -86,7 +81,7 @@ func handleWS(w http.ResponseWriter, r *http.Request) {
 		var update Update
 		err := conn.ReadJSON(&update)
 		if err != nil {
-			fmt.Println(err)
+			fmt.Println("Invalid JSON recieved:", err)
 			delete(clients, conn)
 			return
 		}
