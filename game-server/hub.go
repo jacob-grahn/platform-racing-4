b00298b978785847254305b3a01f06f5ad18f970
Jacob Grahn <jacob@grahn.io>
2024-11-16 09:08:17 -0500

    move room stuff to main thread

--- game-server/hub.go
+++ game-server/hub.go
@@ -26,6 +26,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"time"
 )
 
 // Hub maintains the set of active clients and broadcasts messages to the
@@ -82,6 +83,9 @@ func (h *Hub) run() {
 				continue
 			}
 
+			// do room stuff
+			h.handleUpdate(update)
+
 			for client := range h.clients {
 				// skip this client if this is meant for a specific user and it is not this user
 				if update.TargetUserID != "" && update.TargetUserID != client.ID {
@@ -117,4 +121,118 @@ func (h *Hub) run() {
 			}
 		}
 	}
+}
+
+func (h *Hub) handleUpdate(update *Update) {
+	fmt.Println("Received update: Module(", update.Module+"),  From("+update.ID+")")
+
+	switch Module(update.Module) {
+	case JoinEditorModule:
+		h.handleJoinEditorModule(update)
+	case HostEditorModule:
+		h.handleHostEditorModule(update)
+		update.TargetUserID = update.ID
+	case RequestEditorModule:
+		h.handleRequestEditorModule(update)
+	case ResponseEditorModule:
+		h.handleResponseEditorModule(update)
+	case RequestRoomModule:
+		h.handleRequestRoomModule(update)
+	case EditorModule:
+		h.handleEditorModule(update)
+	}
+}
+
+func (h *Hub) handleJoinEditorModule(update *Update) {
+	room, found := h.findRoom(update.Room)
+	if !found {
+		h.setError(update, RoomNotFoundErrorMessage, update.ID)
+		return
+	}
+
+	room.MembersID = append(room.MembersID, update.ID)
+
+	update.Module = string(JoinSuccessModule)
+	update.MemberIDList = room.MembersID
+	update.HostID = room.HostID
+}
+
+func (h *Hub) handleHostEditorModule(update *Update) {
+	if update.Room == "" {
+		h.setError(update, RoomNotFoundErrorMessage, update.ID)
+		return
+	}
+
+	if _, exists := h.findRoom(update.Room); exists {
+		h.setError(update, RoomExistsErrorMessage, update.ID)
+		return
+	}
+
+	h.rooms = append(h.rooms, &Room{
+		Name:      update.Room,
+		HostID:    update.ID,
+		MembersID: []string{update.ID},
+	})
+	update.Module = string(HostSuccessModule)
+	update.TargetUserID = update.ID
+}
+
+func (h *Hub) handleRequestEditorModule(update *Update) {
+	if update.Room == "" {
+		h.setError(update, RoomNotFoundErrorMessage, update.ID)
+		return
+	}
+
+	room, found := h.findRoom(update.Room)
+	if !found {
+		h.setError(update, RoomNotFoundErrorMessage, update.ID)
+		return
+	}
+
+	update.TargetUserID = room.HostID
+}
+
+func (h *Hub) handleResponseEditorModule(update *Update) {
+	if update.Room == "" {
+		h.setError(update, RoomNotFoundErrorMessage, update.ID)
+		return
+	}
+
+	_, found := h.findRoom(update.Room)
+	if !found {
+		h.setError(update, RoomNotFoundErrorMessage, update.ID)
+		return
+	}
+}
+
+func (h *Hub) handleRequestRoomModule(update *Update) {
+	room, found := h.findRoom(update.Room)
+	if !found {
+		h.setError(update, RoomNotFoundErrorMessage, update.ID)
+		return
+	}
+	update.Module = string(ResponseRoomModule)
+	update.MemberIDList = room.MembersID
+	update.HostID = room.HostID
+}
+
+func (h *Hub) handleEditorModule(update *Update) {
+	if update.Editor == nil {
+		update.Editor = &LevelEditorUpdate{}
+	}
+	update.Editor.Timestamp = time.Now().UnixMilli()
+}
+
+func (h *Hub) findRoom(roomName string) (*Room, bool) {
+	for i := range h.rooms {
+		if h.rooms[i].Name == roomName {
+			return h.rooms[i], true
+		}
+	}
+	return nil, false
+}
+
+func (h *Hub) setError(update *Update, errorMessage ErrorMessage, targetID string) {
+	update.ErrorMessage = string(errorMessage)
+	update.TargetUserID = targetID
 }
