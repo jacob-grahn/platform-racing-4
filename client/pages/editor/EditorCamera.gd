9e5dbf035f5b060bbde533ddeb7646d3d0132cf9
CamerTheDragon <75085825+CamerTheDragon@users.noreply.github.com>
2025-01-09 02:40:24 +0000

    Allow Editor Camera to detect Zoom Panel input

--- client/pages/editor/EditorCamera.gd
+++ client/pages/editor/EditorCamera.gd
@@ -2,11 +2,11 @@ extends Camera2D
 
 var velocity = 2000
 var camera_zoom = 1.0
+var target_zoom = 1.0
+var is_zooming : bool = false
 var camera_speed_multiplier = 1.0
 
-
 func _ready():
-	pass # Replace with function body.
 	set_position_smoothing_enabled(true)
 	set_position_smoothing_speed(7)
 
@@ -23,3 +23,15 @@ func _process(delta):
 	else:
 		camera_speed_multiplier = 1.0
 	position += control_vector * (velocity * camera_speed_multiplier) * delta
+	
+	if is_zooming:
+		var factor_to_zoom = target_zoom / camera_zoom
+		camera_zoom = lerp(camera_zoom, target_zoom, factor_to_zoom*delta*5)
+		if abs(factor_to_zoom - 1) < 0.001:
+			camera_zoom = target_zoom
+			is_zooming = false
+
+
+func _change_camera_zoom(new_zoom_value):
+	is_zooming = true
+	target_zoom = new_zoom_value
