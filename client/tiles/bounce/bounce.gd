ed38d07e7bd3b21811a2c6c782f01e217eac44c3
Jeremiah <jqrqmiqh@gmail.com>
2025-06-26 17:59:08 -0400

    Add files via upload

--- client/tiles/bounce/bounce.gd
+++ client/tiles/bounce/bounce.gd
@@ -13,12 +13,12 @@ func bounce(player: Node2D, tilemap: TileMap, coords: Vector2i) -> void:
 	var tile_position_local = (coords * Settings.tile_size) + Settings.tile_size_half
 	var tile_position_global = tilemap.to_global(tile_position_local)
 	
-	if is_moving_towards(player.position, player.previous_velocity, tile_position_global):
+	if is_moving_towards(player.position, player.movement.previous_velocity, tile_position_global):
 		# bounce, invert velocity
-		player.velocity = player.previous_velocity.bounce(player.last_collision.get_normal())
+		player.velocity = player.movement.previous_velocity.bounce(player.tile_interaction.last_collision.get_normal())
 		
 		# add extra velocity
-		player.velocity = player.velocity * (Vector2(1, 1) + (Vector2(BOUNCINESS, BOUNCINESS) * player.last_collision.get_normal().abs()))
+		player.velocity = player.velocity * (Vector2(1, 1) + (Vector2(BOUNCINESS, BOUNCINESS) * player.tile_interaction.last_collision.get_normal().abs()))
 		
 		# need a speed limit to keep bouncing back and forth from getting out of hand
 		player.velocity = player.velocity.limit_length(3000)
