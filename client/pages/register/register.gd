ed5e92e242c7847463bdc8dbf5c2e7f75613ce65
Jeremiah <jqrqmiqh@gmail.com>
2025-07-12 16:51:04 -0400

    Add files via upload

--- client/pages/register/register.gd
+++ client/pages/register/register.gd
@@ -4,13 +4,13 @@ extends Control
 @onready var password_edit = $Panel/VBoxContainer/PasswordEdit
 @onready var email_edit = $Panel/VBoxContainer/EmailEdit
 @onready var register_button = $Panel/VBoxContainer/RegisterButton
-@onready var login_button = $Panel/VBoxContainer/LoginButton
+@onready var cancel_button = $Panel/VBoxContainer/CancelButton
 @onready var error_label = $Panel/VBoxContainer/ErrorLabel
 
 
 func _ready():
 	register_button.pressed.connect(_on_register_pressed)
-	login_button.pressed.connect(_on_login_pressed)
+	cancel_button.pressed.connect(_on_cancel_pressed)
 	Session.login_failure.connect(_on_register_failure) # Re-using login_failure for simplicity
 
 
@@ -21,8 +21,8 @@ func _on_register_pressed():
 	Session.register(nickname, password, email)
 
 
-func _on_login_pressed():
-	Main.set_scene(Main.LOGIN)
+func _on_cancel_pressed():
+	Main.set_scene(Main.TITLE)
 
 
 func _on_register_failure(error_message):
