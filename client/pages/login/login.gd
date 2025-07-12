5cf14c77270de0bc56cab9ca55f95b22e9bee4e5
Jeremiah <jqrqmiqh@gmail.com>
2025-07-12 16:50:52 -0400

    Add files via upload

--- client/pages/login/login.gd
+++ client/pages/login/login.gd
@@ -3,13 +3,13 @@ extends Control
 @onready var nickname_edit = $Panel/VBoxContainer/NicknameEdit
 @onready var password_edit = $Panel/VBoxContainer/PasswordEdit
 @onready var login_button = $Panel/VBoxContainer/LoginButton
-@onready var register_button = $Panel/VBoxContainer/RegisterButton
+@onready var cancel_button = $Panel/VBoxContainer/CancelButton
 @onready var error_label = $Panel/VBoxContainer/ErrorLabel
 
 
 func _ready():
 	login_button.pressed.connect(_on_login_pressed)
-	register_button.pressed.connect(_on_register_pressed)
+	cancel_button.pressed.connect(_on_cancel_pressed)
 	Session.login_success.connect(_on_login_success)
 	Session.login_failure.connect(_on_login_failure)
 
@@ -20,8 +20,8 @@ func _on_login_pressed():
 	Session.login(nickname, password)
 
 
-func _on_register_pressed():
-	Main.set_scene(Main.REGISTER)
+func _on_cancel_pressed():
+	Main.set_scene(Main.TITLE)
 
 
 func _on_login_success():
