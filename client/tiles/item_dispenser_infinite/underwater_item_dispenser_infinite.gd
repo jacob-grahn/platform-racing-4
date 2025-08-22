b4eb6ad2ae3cab07dcd7dd8d2342d62d77aca3a6
Jeremiah <jqrqmiqh@gmail.com>
2025-08-22 19:18:44 -0400

    Add files via upload

---
+++ client/tiles/item_dispenser_infinite/underwater_item_dispenser_infinite.gd
@@ -0,0 +1,7 @@
+extends ItemDispenser
+class_name UnderwaterItemDispenserInfinite
+
+
+func dispense_item(player: Node2D, tile_map_layer: TileMapLayer, coords: Vector2i):
+	item_id = randi_range(1, 14)
+	player.set_item(item_id)
