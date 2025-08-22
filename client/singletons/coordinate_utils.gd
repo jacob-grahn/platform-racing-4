0b66a6f91195f3e08630875ae1faceceb853aae8
Jeremiah <jqrqmiqh@gmail.com>
2025-08-22 17:35:04 -0400

    Add files via upload

--- client/singletons/coordinate_utils.gd
+++ client/singletons/coordinate_utils.gd
@@ -1,14 +1,32 @@
 extends Node
 ## Provides utility functions for coordinate conversions.
 
+var tiles: Tiles = Tiles.new() # references stuff from tiles script
+var rows: int = tiles.rows
+var columns: int = tiles.columns
+var total_blocks_in_atlas_coords: int = tiles.total_blocks_in_atlas_coords
+var seperator: int = tiles.blocks_for_each_category
+
+
 func to_atlas_coords(block_id: int) -> Vector2i:
 	if block_id == 0:
 		return Vector2i(-1, -1)
-	var id := block_id - 1
-	var x := id % 10
-	var y := id / 10
+	var true_block_id: int = block_id
+	var style_counter: int = 0
+	while true_block_id > seperator:
+		true_block_id -= seperator
+		style_counter += 1
+	var id := (true_block_id + (total_blocks_in_atlas_coords * style_counter)) - 1
+	var x := id % rows
+	var y := id / rows
 	return Vector2i(x, y)
 
 
 func to_block_id(atlas_coords: Vector2i) -> int:
-	return (atlas_coords.y * 10) + atlas_coords.x + 1
+	var block_id = (atlas_coords.y * rows) + atlas_coords.x + 1
+	var style_counter: int = 0
+	while block_id > total_blocks_in_atlas_coords:
+		block_id -= total_blocks_in_atlas_coords
+		style_counter += 1
+	var true_block_id = (block_id + (seperator * style_counter))
+	return int(true_block_id)
--- client/singletons/file_manager.gd
+++ client/singletons/file_manager.gd
@@ -3,14 +3,34 @@ extends Node
 
 var save_dir: String = "user://editor"
 var current_level_name: String = "first"
+var current_level_description: String = ""
 
 
 func get_current_level_name() -> String:
 	return current_level_name
 
 
+func get_current_level_description() -> String:
+	return current_level_description
+
+
 func get_level_file_name(level_name: String) -> String:
 	return "/" + level_name + ".json"
+
+func get_level_description(level_name: String) -> String:
+	var file_name := get_level_file_name(level_name)
+	var save_file_path: String = save_dir + file_name
+	if not FileAccess.file_exists(save_file_path):
+		return ""
+
+	var file := FileAccess.open(save_file_path, FileAccess.READ)
+	var level: Dictionary = JSON.parse_string(file.get_as_text())
+	var description = level.get("description")
+	var description_text = str(description)
+	if !description:
+		return ""
+	else:
+		return description_text
 
 
 func list_saved_levels() -> Array:
@@ -26,13 +46,17 @@ func list_saved_levels() -> Array:
 				filenames.append(file_name.get_basename())
 		dir.list_dir_end()
 	return filenames
-
+	
 
 func set_current_level_name(level_name: String) -> void:
 	current_level_name = level_name
 
 
-func save_to_file(level: Dictionary, level_name: String = current_level_name) -> String:
+func set_current_level_description(level_description: String) -> void:
+	current_level_description = level_description
+
+
+func save_to_file(level: Dictionary, level_name: String) -> String:
 	if level_name == "":
 		return ""
 	
