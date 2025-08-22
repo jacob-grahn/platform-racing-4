f528edb2e8963c5b9676dc6b62b4ee1c11b10518
Jeremiah <jqrqmiqh@gmail.com>
2025-08-22 19:14:46 -0400

    Add files via upload

---
+++ client/tiles/finish/finish.gd
@@ -0,0 +1,17 @@
+class_name Finish
+extends Tile
+## Finish tile that ends the level when a player touches it
+##
+## When a player reaches this tile, the level is completed
+## and the game transitions to the finish state.
+
+
+func init() -> void:
+	matter_type = Tile.ACTIVE
+	is_safe = true
+	bump.push_back(finish)
+
+
+func finish(player: Node2D, tile_map_layer: TileMapLayer, coords: Vector2i) -> void:
+	if !player.movement.finished:
+		player.movement.finished = true
---
+++ client/tiles/finish/industrial_finish.gd
@@ -0,0 +1,12 @@
+extends Finish
+class_name IndustrialFinish
+## Finish tile that ends the level when a player touches it
+##
+## When a player reaches this tile, the level is completed
+## and the game transitions to the finish state.
+
+
+func init() -> void:
+	matter_type = Tile.ACTIVE
+	is_safe = true
+	bump.push_back(finish)
