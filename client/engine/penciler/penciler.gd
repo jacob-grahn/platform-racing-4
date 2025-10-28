22079354435d864df911d9b0c97df8b5b2b1d6c8
Jeremiah <jqrqmiqh@gmail.com>
2025-10-28 11:45:38 -0400

    Add files via upload

--- client/engine/penciler/penciler.gd
+++ client/engine/penciler/penciler.gd
@@ -42,9 +42,9 @@ func _on_level_event(event: Dictionary) -> void:
 			_set_tile(event, coords, coords_key)
 
 	if event.type == EditorEvents.ADD_LINE:
-		var layer = layers.get_node(event.layer_name)
+		var layer = layers.art_layers.get_node(event.layer_name)
 		if layer.visible:
-			var lines: Node2D = layers.get_node(event.layer_name + "/Lines")
+			var lines: Node2D = layers.art_layers.get_node(event.layer_name + "/Lines")
 			var line = Line2D.new()
 			lines.add_child(line)
 			line.end_cap_mode = Line2D.LINE_CAP_ROUND
@@ -85,52 +85,105 @@ func _on_level_event(event: Dictionary) -> void:
 		layer.get_node("TileMapLayer").rotation_degrees = event.get("rotation", 0)
 		layer.set_depth(event.get("depth", 10))
 	
+	if event.type == EditorEvents.ADD_BLOCK_LAYER:
+		var layer := layers.add_block_layer(event.name)
+		layer.set_block_layer_rotation(event.get("tile_map_rotation", 0))
+		layer.set_z_axis(event.get("z_axis", 10))
+	
+	if event.type == EditorEvents.ADD_ART_LAYER:
+		var layer := layers.add_art_layer(event.name)
+		layer.art_scale = event.get("art_scale", 1.0)
+		layer.set_art_rotation(event.get("art_rotation", 10))
+		layer.set_depth(event.get("depth", 10))
+		layer.set_z_axis(event.get("z_axis", 10))
+		layer.set_art_alpha(event.get("alpha", 100))
+	
 	if event.type == EditorEvents.RENAME_LAYER:
 		var layer := layers.get_node(event.layer_name)
+		layer.name = event.new_layer_name
+	
+	if event.type == EditorEvents.RENAME_BLOCK_LAYER:
+		var layer := layers.block_layers.get_node(event.layer_name)
+		layer.name = event.new_layer_name
+	
+	if event.type == EditorEvents.RENAME_ART_LAYER:
+		var layer := layers.art_layers.get_node(event.layer_name)
 		layer.name = event.new_layer_name
 
 	if event.type == EditorEvents.DELETE_LAYER:
 		layers.remove_layer(event.name)
+	
+	if event.type == EditorEvents.DELETE_BLOCK_LAYER:
+		layers.remove_block_layer(event.name)
+	
+	if event.type == EditorEvents.DELETE_ART_LAYER:
+		layers.remove_art_layer(event.name)
 
-	if event.type == EditorEvents.ADD_USERTEXT:
-		var layer = layers.get_node(event.layer_name)
+	if event.type == EditorEvents.ADD_TEXT:
+		var layer = layers.art_layers.get_node(event.layer_name)
 		if layer.visible:
-			var usertextboxes: Node2D = layers.get_node(event.layer_name + "/UserTextboxes")
-			var usertextbox_scene: PackedScene = preload("res://engine/textbox/user_textbox.tscn")
-			var usertextbox = usertextbox_scene.instantiate()
+			var textboxes: Node2D = layers.art_layers.get_node(event.layer_name + "/Texts")
+			var textbox_scene: PackedScene = preload("res://engine/textbox/textbox.tscn")
+			var textbox = textbox_scene.instantiate()
 			var grab_focus: bool
 			if event.has("dont_grab_focus") and event.dont_grab_focus:
 				grab_focus = false
 			else:
 				grab_focus = true
-			usertextboxes.add_child(usertextbox)
-			usertextbox.set_usertext_properties(event.usertext, event.font, event.font_size, Vector2(event.width, event.height), grab_focus)
-			usertextbox.position = Vector2(event.position.x, event.position.y)
-			usertextbox.rotation = event.text_rotation
-		
-			# Handle additional properties if provided
-			# if event.has("text_width") and event.has("text_height"):
-			#	usertextbox.resize_text(event.text_width, event.text_height)
+			textboxes.add_child(textbox)
+			textbox.set_text_properties(event.text, event.font, event.font_size, Vector2(event.width, event.height), grab_focus)
+			textbox.position = Vector2(event.position.x, event.position.y)
+			textbox.rotation = event.text_rotation
 		
 			# Configure mouse interaction based on editing mode
 			if event.has("is_editing"):
 				if event.is_editing:
-					usertextbox.mouse_filter = 0 # Editable on click (click stops at text)
+					textbox.mouse_filter = 0 # Editable on click (click stops at text)
 				else:
-					usertextbox.mouse_filter = 2 # Not Editable on click (click passes through)
-				usertextbox.disable_text_edits()
+					textbox.mouse_filter = 2 # Not Editable on click (click passes through)
+				textbox.disable_text_edits()
 
-	if event.type == EditorEvents.ROTATE_LAYER:
+	if event.type == EditorEvents.SET_LAYER_Z_AXIS:
 		var layer = layers.get_node(event.layer_name)
-		layer.get_node("TileMapLayer").rotation_degrees = event.rotation
-
-	if event.type == EditorEvents.LAYER_DEPTH:
+		layer.z_axis = event.z_axis
+	
+	if event.type == EditorEvents.SET_BLOCK_LAYER_Z_AXIS:
+		var layer = layers.block_layers.get_node(event.layer_name)
+		layer.set_z_axis(event.z_axis)
+	
+	if event.type == EditorEvents.SET_ART_LAYER_Z_AXIS:
+		var layer = layers.art_layers.get_node(event.layer_name)
+		layer.z_axis = event.z_axis
+	
+	if event.type == EditorEvents.SET_LAYER_DEPTH:
 		var layer = layers.get_node(event.layer_name)
 		layer.set_depth(event.depth)
+	
+	if event.type == EditorEvents.SET_ART_LAYER_DEPTH:
+		var layer = layers.art_layers.get_node(event.layer_name)
+		layer.set_depth(event.depth)
 
-	if event.type == EditorEvents.VISIBLE_LAYER:
+	if event.type == EditorEvents.SET_LAYER_ROTATION:
 		var layer = layers.get_node(event.layer_name)
-		layer.visible = event.visibility
+		layer.get_node("TileMapLayer").rotation_degrees = event.rotation
+	
+	if event.type == EditorEvents.SET_BLOCK_LAYER_ROTATION:
+		var layer = layers.block_layers.get_node(event.layer_name)
+		layer.set_block_layer_rotation(event.tile_map_rotation)
+	
+	if event.type == EditorEvents.SET_ART_LAYER_ROTATION:
+		var layer = layers.art_layers.get_node(event.layer_name)
+		layer.get_node("Stamps").rotation_degrees = event.rotation
+		layer.get_node("Lines").rotation_degrees = event.rotation
+		layer.get_node("Texts").rotation_degrees = event.rotation
+	
+	if event.type == EditorEvents.SET_LAYER_ALPHA:
+		var layer = layers.get_node(event.layer_name)
+		layer.alpha = event.alpha
+	
+	if event.type == EditorEvents.SET_ART_LAYER_ALPHA:
+		var layer = layers.art_layers.get_node(event.layer_name)
+		layer.alpha = event.alpha
 
 	if event.type == EditorEvents.SET_BACKGROUND:
 		if bg:
@@ -138,12 +191,16 @@ func _on_level_event(event: Dictionary) -> void:
 				bg.set_bg(event.bg, event.fade_color)
 			else:
 				bg.set_bg(event.bg, Color(event.fade_color))
+	
+	if event.type == EditorEvents.SET_SONG_ID:
+		if bg:
+			bg.set_song_id(event.song_id)
 
 
 func _set_tile(event: Dictionary, coords: Vector2i, coords_key: String, new_timestamp: int = -1) -> void:
-	var layer = layers.get_node(event.layer_name)
+	var layer = layers.block_layers.get_node(event.layer_name)
 	if layer.visible:
-		var tile_map_layer: TileMapLayer = layers.get_node(event.layer_name + "/TileMapLayer")
+		var tile_map_layer: TileMapLayer = layers.block_layers.get_node(event.layer_name + "/TileMapLayer")
 		tile_map_layer.set_cell(coords, 0, CoordinateUtils.to_atlas_coords(event.block_id))
 	
 	if new_timestamp != -1:
--- client/engine/penciler/rastertest.gd
+++ client/engine/penciler/rastertest.gd
@@ -1,12 +1,20 @@
 extends Node2D
 
 @onready var bg = $Background
-var image = Image.new()
-var image_texture = ImageTexture.new()
+@onready var cursor = $Cursor
+var image = Image.new() #.load_from_file("res://stamps/CactusGraphic.svg")
 
 
 func _ready() -> void:
-	image.create_empty(1920, 1080, false, Image.FORMAT_RGBA8)
-	image.fill(Color(1, 1, 1, 0.5))
-	image_texture.create_from_image(image)
-	bg.texture = image_texture
+	bg.texture = ImageTexture.new()
+	image = Image.create_empty(1920, 1080, false, Image.FORMAT_RGBA8)
+	#image.fill(Color("FFFFFFFF"))
+	#image.set_pixel(60, 60, Color.RED)
+	bg.texture.set_image(image)
+	global_position = Vector2(image.get_width() / 2, image.get_height() / 2)
+
+func _process(delta: float) -> void:
+	cursor.global_position = get_global_mouse_position()
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		image.set_pixel(int(cursor.global_position.x), int(cursor.global_position.y), Color.RED)
+	bg.texture.update(image)
