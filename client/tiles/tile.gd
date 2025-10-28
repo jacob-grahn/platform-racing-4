906e1d7a82ee6440693bad8bbaacc39d037b3f1c
Jeremiah <jqrqmiqh@gmail.com>
2025-10-28 17:56:55 -0400

    Add files via upload

--- client/tiles/tile.gd
+++ client/tiles/tile.gd
@@ -32,6 +32,7 @@ var stand := [] # could be any side depending on player rotation
 var bump := [] # could be any side depending on player rotation
 var tick := [] # will run on some interval
 var area := [] # used for tiles with no collision like water
+var options_menu = null
 var physics_type := STATIC
 var matter_type := ACTIVE
 var block_side_settings := {"type": ACTIVE, "top": [], "bottom": [], "left": [], "right": [], "bump": [],}
@@ -40,6 +41,7 @@ var cooldown_dict := {}
 var default_change_list: Array = [1, 5, 18, 27, 16, 26, 11, 10, 23, 22, 28, 25, 19, 21, 33, 32, 34, 13, 7, 6, 8, 9, 29, 30, 37, 38, 39]
 var default_change_frequency: float = 2.5
 var default_move_frequency: float = 2.5
+var default_custom_stats: Array = [50, 50, 50, 50]
 
 
 func init() -> void:
@@ -92,6 +94,11 @@ func is_active(tile_map_layer: TileMapLayer, coords: Vector2i) -> bool:
 func is_visible(tile_map_layer: TileMapLayer, coords: Vector2i) -> bool:
 	var alt_id := tile_map_layer.get_cell_alternative_tile(coords)
 	return alt_id == Tile.DEACTIVATED_ALT_ID or alt_id == Tile.VISIBLE_ALT_ID
+
+
+func has_options_menu(tile_map_layer: TileMapLayer, coords: Vector2i) -> bool:
+	var tile = tile_map_layer.get_cell_tile_data(coords)
+	return tile.options_menu != null
 
 
 func get_slug(tile_map_layer: TileMapLayer, coords: Vector2i) -> String:
