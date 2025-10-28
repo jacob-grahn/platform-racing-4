72571d3a83a40fd68557008da2e8d436802e3074
Jeremiah <jqrqmiqh@gmail.com>
2025-10-28 11:42:37 -0400

    Add files via upload

---
+++ client/engine/stamp/stamps.gd
@@ -0,0 +1,18 @@
+extends Node
+class_name Stamps
+
+const cactus = preload("res://engine/stamp/cactus-graphic.svg")
+const petrifiedtree = preload("res://engine/stamp/petrifiedtree-graphic.svg")
+const rock2 = preload("res://engine/stamp/rock2-graphic.svg")
+const rock = preload("res://engine/stamp/rock-graphic.svg")
+const skyscraper = preload("res://engine/stamp/skyscraper-graphic.svg")
+const spire2 = preload("res://engine/stamp/spire2-graphic.svg")
+const spire = preload("res://engine/stamp/spire-graphic.svg")
+const tree2 = preload("res://engine/stamp/tree2-graphic.svg")
+const tree3 = preload("res://engine/stamp/tree3-graphic.svg")
+const tree = preload("res://engine/stamp/tree-graphic.svg")
+var stamp_list: Array = ["cactus", "petrifiedtree", "rock2", "rock", "skyscraper",
+"spire2", "spire", "tree2", "tree3", "tree"]
+var stamp_graphic_list: Array = [cactus, petrifiedtree, rock2, rock, skyscraper,
+spire2, spire, tree2, tree3, tree]
+# ^ this is for level editor ^
