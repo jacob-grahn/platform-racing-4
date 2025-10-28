125dbf4cd90260a8922e0c3a5d3a04c1129e0656
Jeremiah <jqrqmiqh@gmail.com>
2025-10-28 17:44:48 -0400

    Add files via upload

--- client/tiles/item_dispenser_infinite/item_dispenser_infinite.gd
+++ client/tiles/item_dispenser_infinite/item_dispenser_infinite.gd
@@ -5,3 +5,4 @@ class_name ItemDispenserInfinite
 func dispense_item(player: Node2D, tile_map_layer: TileMapLayer, coords: Vector2i):
 	item_id = randi_range(1, 14)
 	player.set_item(item_id)
+	Jukebox.play_sound("star")
